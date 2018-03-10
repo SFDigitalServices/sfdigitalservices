3dfabbd0e9de10f53f18726a23db71a5fb032581
Anthony Kong <anthony.kong@sfgov.org>
2018-03-09 16:36:13 -0800

    add blog link to header and footer and hamburgerize the menu

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,22 +1,22 @@
-// $('#ham, #menu-back').click(function(e) {
-//   e.preventDefault();
-//   if($('#menu').hasClass('in')) {
-//     $('#menu').removeClass('in');
-//     setTimeout(function() {
-//       $('#menu').css({'height':'auto'});
-//     }, 300);
-//   } else {
-//     var wHeight = $(window).height() + 'px';
-//     $('#menu').addClass('in');
-//     $('#menu').css({'height': wHeight });
-//     // $('#nav-overlay').css({'height': wHeight, 'display': 'block' });
-//   }
-// });
+$('#ham, #menu-back').click(function(e) {
+  e.preventDefault();
+  if($('#menu').hasClass('in')) {
+    $('#menu').removeClass('in');
+    setTimeout(function() {
+      $('#menu').css({'height':'auto'});
+    }, 300);
+  } else {
+    var wHeight = $(window).height() + 'px';
+    $('#menu').addClass('in');
+    $('#menu').css({'height': wHeight });
+    // $('#nav-overlay').css({'height': wHeight, 'display': 'block' });
+  }
+});
 
-// $(window).resize(function() {
-//   var width = $(this).width();
-//   if(width > 768) {
-//     $('#menu').removeClass('in');
-//     $('#menu').css({'height':'auto'});
-//   }
-// });
+$(window).resize(function() {
+  var width = $(this).width();
+  if(width > 768) {
+    $('#menu').removeClass('in');
+    $('#menu').css({'height':'auto'});
+  }
+});
