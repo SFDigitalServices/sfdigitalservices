031c56b61b690704be0419a4fe4c295469a6f35e
Anthony Kong <anthony.kong@sfgov.org>
2019-07-24 14:02:39 -0700

    circleci test deploy

---
+++ .circleci/scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
+
+  git config --global user.email $GH_EMAIL
+  git config --global user.name $GH_NAME
+
+  git clone $CIRCLE_REPOSITORY_URL gh-pages
+
+  cd gh-pages
+  git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+  git rm -rf .
+  cd ..
+  
+  cp -a hugo/public/. gh-pages/.
+  
+  mkdir -p gh-pages/.circleci && cp -a hugo/.circleci/. gh-pages/.circleci/. # copy circleci config to ignore triggering builds when pushing to gh-pages
+  cd gh-pages
+
+  git add -A
+  git commit -m "Automated deploy to gh pages: ${CIRCLE_SHA1}" --allow-empty
+
+  git push origin $TARGET_BRANCH
+
+fi
