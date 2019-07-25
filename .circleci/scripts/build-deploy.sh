56938f44ca6f45c0e5a520d0db056bd9d409b2ec
Anthony Kong <anthony.kong@sfgov.org>
2019-07-25 00:58:02 -0700

    circleci delete multidev after merge, terminus logout

--- .circleci/scripts/build-deploy.sh
+++ .circleci/scripts/build-deploy.sh
@@ -30,4 +30,7 @@ git push -f pantheon $CIRCLE_BRANCH:ci-$CIRCLE_BUILD_NUM
 if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
   # merge multidev to master
   terminus multidev:merge-to-dev $PANTHEON_SITENAME.ci-$CIRCLE_BUILD_NUM
+  terminus multidev:delete --delete-branch $PANTHEON_SITENAME.ci-$CIRCLE_BUILD_NUM
 fi
+
+terminus auth:logout
