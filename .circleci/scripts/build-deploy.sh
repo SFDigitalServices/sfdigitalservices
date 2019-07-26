ac8fcdec13f091c4e12bc943f54a6e8074c5e95c
Anthony Kong <anthony.kong@sfgov.org>
2019-07-26 15:49:16 -0700

    circleci create multidev if not source branch (master)

--- .circleci/scripts/build-deploy.sh
+++ .circleci/scripts/build-deploy.sh
@@ -8,28 +8,27 @@ git config --global user.email $GH_EMAIL
 git config --global user.name $GH_NAME
 
 git clone $CIRCLE_REPOSITORY_URL $CIRCLE_BRANCH
-cd $CIRCLE_BRANCH
-git checkout $CIRCLE_BRANCH || git checkout --orphan $CIRCLE_BRANCH
-HUGO_ENV=production hugo -v
-git rm -rf .
-mv public/* .
-git add -A
-git commit -m "build ${CIRCLE_BRANCH} to pantheon remote ci-${CIRCLE_BUILD_NUM}: ${CIRCLE_SHA1}" --allow-empty
-
-git remote add pantheon $PANTHEON_REMOTE
-
-terminus -n auth:login --machine-token="$TERMINUS_MACHINE_TOKEN"
-terminus multidev:create $PANTHEON_SITENAME.dev ci-$CIRCLE_BUILD_NUM
 
 ssh-add -D
 ssh-add ~/.ssh/id_rsa_$PANTHEON_SSH_FINGERPRINT
 ssh-keyscan -H -p $PANTHEON_CODESERVER_PORT $PANTHEON_CODESERVER >> ~/.ssh/known_hosts
 
-git push -f pantheon $CIRCLE_BRANCH:ci-$CIRCLE_BUILD_NUM
+cd $CIRCLE_BRANCH
+git checkout $CIRCLE_BRANCH || git checkout --orphan $CIRCLE_BRANCH
+HUGO_ENV=production hugo -v
+git rm -rf .
+mv public/* .
+git remote add pantheon $PANTHEON_REMOTE
+git add -A
 
 if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
+  git commit -m "build ${CIRCLE_BRANCH} to pantheon: ${CIRCLE_SHA1}" --allow-empty
   git push -f pantheon $CIRCLE_BRANCH:master
+else
+  git commit -m "build ${CIRCLE_BRANCH} to pantheon remote ci-${CIRCLE_BUILD_NUM}: ${CIRCLE_SHA1}" --allow-empty
+  terminus -n auth:login --machine-token="$TERMINUS_MACHINE_TOKEN"
+  terminus multidev:create $PANTHEON_SITENAME.dev ci-$CIRCLE_BUILD_NUM
+  git push -f pantheon $CIRCLE_BRANCH:ci-$CIRCLE_BUILD_NUM
   terminus multidev:delete --delete-branch $PANTHEON_SITENAME.ci-$CIRCLE_BUILD_NUM -y
+  terminus auth:logout
 fi
-
-terminus auth:logout
