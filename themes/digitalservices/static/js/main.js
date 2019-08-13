11d321225a9116fb3b50d87a1ad91466df817612
Anthony Kong <anthony.kong@sfgov.org>
2019-08-13 11:40:22 -0700

    shift logo when skip link active if necessary

--- themes/digitalservices/static/js/main.js
+++ themes/digitalservices/static/js/main.js
@@ -38,7 +38,9 @@ function resetMenu() {
   });
   $('#skip-link').on('focus', function() {
     $(this).addClass('skip-link-active');
-    $('.navbar-brand.logo').css({'margin-left': (parseFloat($('.navbar-brand.logo').css('margin-left').replace('px','')) + $(this).width()) + 'px'})
+    var minLeftPos = $('#skip-link').position().left + $('#skip-link').width();
+    var logoLeftPos = $('.navbar-brand.logo').position().left;
+    if(logoLeftPos < minLeftPos) $('.navbar-brand.logo').css({'margin-left': (parseFloat($('.navbar-brand.logo').css('margin-left').replace('px','')) + $(this).width()) + 'px'});
   });
   $('#skip-link').on('blur', function() {
     $(this).removeClass('skip-link-active');
