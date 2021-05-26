aa18bdb709896d32d29686c4baf52f966b1101b9
Anthony Kong <anthony.kong@sfgov.org>
2021-05-25 18:02:51 -0700

    cannot create multidevs on pantheon for this static site anymore

--- .circleci/scripts/build-deploy.sh
+++ .circleci/scripts/build-deploy.sh
@@ -26,29 +26,12 @@ cp -a ../tmp/.circleci . # copy circleci config back to prevent triggering build
 git remote add pantheon $PANTHEON_REMOTE
 git add -A
 
+terminus -n auth:login --machine-token="$TERMINUS_MACHINE_TOKEN"
+git push -f pantheon $CIRCLE_BRANCH:master
+
 if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
   git commit -m "build ${CIRCLE_BRANCH} ${CIRCLE_SHA1}" --allow-empty
-  git push -f pantheon $CIRCLE_BRANCH:master
   git push -f origin $CIRCLE_BRANCH:gh-pages
-else
-  git commit -m "build ${CIRCLE_BRANCH} to pantheon remote ci-${CIRCLE_BUILD_NUM}: ${CIRCLE_SHA1}" --allow-empty
-  terminus -n auth:login --machine-token="$TERMINUS_MACHINE_TOKEN"
-
-  # do some cleanup
-  terminus multidev:list $PANTHEON_SITENAME --format=list --fields=name > multidevs.txt # capture multidevs to file
-  MD_COUNT="$(< multidevs.txt wc -l)" # capture the multidev count (# lines in file from above)
-  if [ $MD_COUNT -gt 5 ]; then
-    echo "Removing older multidevs"
-    # remove first 3 multidevs in file list
-    head -3 multidevs.txt |
-    while read multidev; do
-      terminus multidev:delete  --delete-branch -y -- $PANTHEON_SITENAME.$multidev
-    done
-  else
-    echo "No need to remove multidevs.  Count: $MD_COUNT"
-  fi
-
-  terminus multidev:create $PANTHEON_SITENAME.dev ci-$CIRCLE_BUILD_NUM
-  git push -f pantheon $CIRCLE_BRANCH:ci-$CIRCLE_BUILD_NUM
-  terminus auth:logout
 fi
+
+terminus auth:logout
