5ea17e4585a8665015e26879167bffe001a6e63e
Anthony Kong <anthony.kong@sfgov.org>
2021-05-26 03:47:53 -0700

    move node files to root

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3333
 const path = require('path')
-const public = path.join(__dirname, '../public');
+const public = path.join(__dirname, 'public');
 
 app.get('/test', (req, res) => {
   res.send('test ok')
